3a6c66b7c59f75389438bb43cfd1e0119ffdabad
Celia <celia.rozalen@gmail.com>
2019-09-04 13:50:42 +0200

    Sidebar and building page layout

---
+++ layout/sidebar.php
@@ -0,0 +1,15 @@
+<div class="col--lg--3 sidebar">
+    <div class="tab">
+        Constructores
+        <div class="profile"></div>
+    </div>
+    <div class="tab">
+        Pisos: <span class=floor>X/X</span>
+    </div>
+    <div class="tab">
+        Mensajes
+        <ul class=msg>
+            <li >¡Acabado Sprint 1!</li>
+        </ul>
+    </div>
+</div>
---
+++ pages/profile.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="../css/styles.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,500,600&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="container fullwidth">
+        <div class="col--lg--9 profile">
+        </div>
+            <?php
+            include '../layout/sidebar.php';
+            ?>
+    </div>
+</body>
+</html>
